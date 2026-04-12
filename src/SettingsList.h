adbaa5efe6aff3b90697db96a4c215101e70d3db
bdeshi <bdeshi@bdeshi.space>
2026-04-13 01:31:26 +0600

    feat: enable manual screen refresh on power button short press (#1626)
    ## Summary
    
    Adds an option to allow manual screen refresh on power button short
    press.
    
    ## Additional Context
    
    there's an option to refresh the screen after a set number of pages. but
    sometimes a manual refresh is needed to clear up stale ink pixels. this
    works everywhere both in and out of reading mode.
    
    resolves #550 
    
    ---
    
    ### AI Usage
    
    While CrossPoint doesn't have restrictions on AI tools in contributing,
    please be transparent about their usage as it helps set the right
    context for reviewers.
    
    Did you use AI tools to help write this code? _**PARTIALLY**_, to
    understand the project structure.

--- src/SettingsList.h
+++ src/SettingsList.h
@@ -152,8 +152,8 @@ inline std::vector<SettingInfo> getSettingsList(const SdCardFontRegistry* regist
       SettingInfo::Toggle(StrId::STR_LONG_PRESS_SKIP, &CrossPointSettings::longPressChapterSkip, "longPressChapterSkip",
                           StrId::STR_CAT_CONTROLS),
       SettingInfo::Enum(StrId::STR_SHORT_PWR_BTN, &CrossPointSettings::shortPwrBtn,
-                        {StrId::STR_IGNORE, StrId::STR_SLEEP, StrId::STR_PAGE_TURN}, "shortPwrBtn",
-                        StrId::STR_CAT_CONTROLS),
+                        {StrId::STR_IGNORE, StrId::STR_SLEEP, StrId::STR_PAGE_TURN, StrId::STR_FORCE_REFRESH},
+                        "shortPwrBtn", StrId::STR_CAT_CONTROLS),
 
       // --- System ---
       SettingInfo::Enum(StrId::STR_TIME_TO_SLEEP, &CrossPointSettings::sleepTimeout,
